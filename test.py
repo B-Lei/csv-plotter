7a82d4a7b1f0a0f31417e401b299381d5995280d
b-lei <benjamin.g.lei@gmail.com>
2018-06-12 13:12:30 -0700

    More unit tests

--- test.py
+++ test.py
@@ -5,14 +5,30 @@ from runme import get_csv_list, get_arguments
 
 class TestCSVPlotter(unittest.TestCase):
 
-    def test_minimum(self):
-        args = get_arguments(['-d', 'placeholder', '-c', 'placeholder'])
+    def test_min_args(self):
+        args = get_arguments(['-d', 'foo', '-c', 'bar'])
         self.assertTrue(args.dir and args.cols)
 
-    def test_default_name_dir(self):
-        args = get_arguments(
-            ['-d', 'ex*/my_examples', '-c', 'hunger', '--xaxis', 'timestamp', '-t', 'Pet Hunger Over Time',
-             '-y', 'Hunger', '-x', 'Time (minutes)', '-I', '-n', 'pet_hunger'])
+    def test_csv_parsing_cat(self):
+        args = get_arguments(['-f', 'ex*/my*/cat*', '-c', 'hunger', '-n', 'pet_hunger', '--xaxis', 'timestamp'])
+        csv = get_csv_list(args)[0]
+        self.assertEquals(csv.numrows, 14)
+        self.assertEquals(len(csv.data), 2)
+
+    def test_csv_parsing_dog(self):
+        args = get_arguments(['-f', 'ex*/my*/dog*', '-c', 'hunger', '-n', 'pet_hunger', '--xaxis', 'timestamp'])
+        csv = get_csv_list(args)[0]
+        self.assertEquals(csv.numrows, 12)
+        self.assertEquals(len(csv.data), 2)
+
+    def test_plotter_default_name_dir(self):
+        args = get_arguments(['-d', 'ex*/my*', '-c', 'hunger', '-n', 'pet_hunger'])
+        csv_list = get_csv_list(args)
+        plot = Plotter(args, csv_list)
+        self.assertEqual(plot.arg['name'], 'pet_hunger.html')
+
+    def test_plotter_arg_filename(self):
+        args = get_arguments(['-d', 'ex*/my*', '-c', 'hunger', '-I'])
         csv_list = get_csv_list(args)
         plot = Plotter(args, csv_list)
         self.assertEqual(plot.arg['name'], 'my_examples.png')
