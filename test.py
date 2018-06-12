317b219ee939bff352d06fc4dc6f6649f62f44a0
b-lei <benjamin.g.lei@gmail.com>
2018-06-12 12:49:50 -0700

    Added example CSV files

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ class TestCSVPlotter(unittest.TestCase):
     def test_default_name_dir(self):
         args = get_arguments(
             ['-d', 'ex*/my_examples', '-c', 'hunger', '--xaxis', 'timestamp', '-t', 'Pet Hunger Over Time',
-             '-y', 'Hunger', '-x', 'Time (minutes)', '-I'])
+             '-y', 'Hunger', '-x', 'Time (minutes)', '-I', '-n', 'pet_hunger'])
         csv_list = get_csv_list(args)
         plot = Plotter(args, csv_list)
         self.assertEqual(plot.arg['name'], 'my_examples.png')
